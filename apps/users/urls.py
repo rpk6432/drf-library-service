f51dca11fcd5721c46c83501ee34cbaa29b14436
Roman Peka <pekaromanmail@gmail.com>
2025-09-29 20:41:57 +0300

    Implement UserViewSet and register user API endpoints

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,13 +1,20 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register("", UserViewSet)
+
 urlpatterns = [
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("token/verify/", TokenObtainPairView.as_view(), name="token_verify"),
+    path("", include(router.urls)),
 ]
 
 app_name = "users"
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    BasePermission,
+)
+from rest_framework.request import Request
+from rest_framework.response import Response
 
-# Create your views here.
+from .serializers import UserSerializer
+
+
+User = get_user_model()
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def get_permissions(self) -> list[BasePermission]:
+        """Instantiates and returns the list of permissions that this view requires."""
+        if self.action == "create":
+            permission_classes = [AllowAny]
+        else:
+            permission_classes = [IsAuthenticated]
+        return [permission() for permission in permission_classes]
+
+    @action(
+        methods=["GET", "PUT", "PATCH"],
+        detail=False,
+        url_path="me",
+        permission_classes=[IsAuthenticated],
+    )
+    def me(self, request: Request) -> Response:
+        """Retrieve or update the authenticated user's profile"""
+        user = request.user
+        if request.method == "GET":
+            serializer = self.get_serializer(user)
+            return Response(serializer.data)
+
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
