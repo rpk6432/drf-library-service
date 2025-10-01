33b77ade07a0cedc82c9d18f41f47c251f59e2c8
Roman Peka <pekaromanmail@gmail.com>
2025-10-01 05:34:57 +0300

    Add Borrowing model to Django admin

--- apps/borrowings/admin.py
+++ apps/borrowings/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Borrowing
+
+
+@admin.register(Borrowing)
+class BorrowingAdmin(admin.ModelAdmin):
+    list_display = (
+        "book",
+        "user",
+        "borrow_date",
+        "expected_return_date",
+        "actual_return_date",
+    )
+    list_filter = ("borrow_date", "expected_return_date", "actual_return_date")
+    search_fields = ("book__title", "user__email")
