cc2bf9c042601206c6a04569f1ef4726bf7e8cab
Roman Peka <pekaromanmail@gmail.com>
2025-10-01 05:34:37 +0300

    Implement list & detail endpoints for borrowings

--- apps/borrowings/views.py
+++ apps/borrowings/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from apps.borrowings.models import Borrowing
+from apps.borrowings.serializers import (
+    BorrowingListSerializer,
+    BorrowingDetailSerializer,
+)
+
+
+class BorrowingViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = BorrowingListSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = Borrowing.objects.all()
+
+        if self.action == "list":
+            return queryset.select_related("book")
+        if self.action == "retrieve":
+            return queryset.select_related("book", "user")
+
+        return queryset
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return BorrowingDetailSerializer
+        return self.serializer_class
