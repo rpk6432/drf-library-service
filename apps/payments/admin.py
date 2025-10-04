c13b29acd4b4dd97e8baa0be27a1d7d7f83cbf2a
Roman Peka <pekaromanmail@gmail.com>
2025-10-05 00:55:58 +0300

    Register Payment model in Django admin

--- apps/payments/admin.py
+++ apps/payments/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "status",
+        "type",
+        "borrowing",
+        "money_to_pay",
+    )
+    list_filter = ("status", "type")
+    search_fields = ("borrowing__user__email", "borrowing__book__title")
