a14bbdb55b1347ac52adb354a964a3fcfa7c7326
Roman Peka <pekaromanmail@gmail.com>
2025-10-08 01:56:23 +0300

    Add documentation to User API endpoints

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.permissions import (
@@ -15,6 +16,14 @@ from .serializers import UserSerializer
 User = get_user_model()
 
 
+@extend_schema_view(
+    list=extend_schema(summary="List users"),
+    retrieve=extend_schema(summary="Retrieve user"),
+    create=extend_schema(summary="Register new user"),
+    update=extend_schema(summary="Update user"),
+    partial_update=extend_schema(summary="Partially update user"),
+    destroy=extend_schema(summary="Delete user"),
+)
 class UserViewSet(
     mixins.ListModelMixin,
     mixins.RetrieveModelMixin,
@@ -26,13 +35,15 @@ class UserViewSet(
     serializer_class = UserSerializer
 
     def get_permissions(self) -> list[BasePermission]:
-        """Instantiates and returns the list of permissions that this view requires."""
         if self.action == "create":
             permission_classes = [AllowAny]
         else:
             permission_classes = [IsAuthenticated]
         return [permission() for permission in permission_classes]
 
+    @extend_schema(methods=["GET"], summary="Retrieve current user")
+    @extend_schema(methods=["PUT"], summary="Update current user")
+    @extend_schema(methods=["PATCH"], summary="Partially update current user")
     @action(
         methods=["GET", "PUT", "PATCH"],
         detail=False,
@@ -40,7 +51,6 @@ class UserViewSet(
         permission_classes=[IsAuthenticated],
     )
     def me(self, request: Request) -> Response:
-        """Retrieve or update the authenticated user's profile"""
         user = request.user
         if request.method == "GET":
             serializer = self.get_serializer(user)
